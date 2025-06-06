ecd942e23785fa28ed51aa1ed12f19fa4318d97f
Dpeveloper <danielpinnilla@gmail.com>
2025-06-06 17:14:18 -0500

    proyecto inicializado

---
+++ invoice-web/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { CustomerComponent } from './core/pages/customer/customer.component';
+
+export const routes: Routes = [
+    {
+    path: '',
+    component: CustomerComponent,
+  },
+];
---
+++ invoice-web/src/app/services/customer-service.service.ts
@@ -0,0 +1,68 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+export interface InvoiceDto {
+  invoiceId: number;
+  date: string;
+  total: number;
+  customer: CustomerDto;
+  InvoiceDetails: InvoiceDetailDto[]
+}
+
+export interface InvoiceDetailDto {
+  id: number;
+  productName: string;
+  quantity: number;
+  unitPrice: number;
+}
+
+export interface CustomerDto {
+  customerId: number;
+  name: string;
+  customerNumber: number;
+  location: string;
+  invoices: InvoiceDto[];
+}
+
+export interface CustomerToSaveDto {
+  name: string;
+  customerNumber: number;
+  location: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  private url = "http://localhost:5253/api/Customer"
+
+  private readonly http = inject(HttpClient); 
+
+  // GET: Obtener todos los clientes
+  getAll(): Observable<CustomerDto[]> {
+    return this.http.get<CustomerDto[]>(this.url);
+  }
+
+  // GET: Obtener un cliente por ID
+  getById(id: number): Observable<CustomerDto> {
+    return this.http.get<CustomerDto>(`${this.url}/${id}`);
+  }
+
+  // POST: Crear nuevo cliente
+  create(customer: CustomerToSaveDto): Observable<CustomerDto> {
+    return this.http.post<CustomerDto>(this.url, customer);
+  }
+
+  // PUT: Actualizar cliente existente
+  update(id: number, customer: CustomerToSaveDto): Observable<void> {
+    return this.http.put<void>(`${this.url}/${id}`, customer);
+  }
+
+  // DELETE: Eliminar cliente por ID
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${id}`);
+  }
+}
