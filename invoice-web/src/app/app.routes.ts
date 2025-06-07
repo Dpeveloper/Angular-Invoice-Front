14b3248446390a5cb89fafb8aa6736ebf010a62a
Mauripolo17 <mauriciopoloem@gmail.com>
2025-06-07 01:46:15 -0500

    Funcionalidad Creacion de ventas

--- invoice-web/src/app/app.routes.ts
+++ invoice-web/src/app/app.routes.ts
@@ -4,6 +4,10 @@ export const routes: Routes = [
     {
     path: '',
     loadComponent:() => import('./pages/customer/customer.component').then(mod => mod.CustomerComponent)
-  },
+    },
+    {
+    path: 'invoice',
+    loadComponent:() => import('./pages/invoice/invoice.component').then(mod => mod.InvoiceFormComponent)
+    },
 
 ];
